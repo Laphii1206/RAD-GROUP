59bf4a86c7d5fa012fe865bfbcc903292fbabb5c
Laphii <91127087+Laphii1206@users.noreply.github.com>
2025-11-04 23:40:39 +0800

    convert all to css style and edit all class name in contact

--- pages/contact.php
+++ pages/contact.php
@@ -5,113 +5,10 @@ session_start();
 
 <!DOCTYPE html>
 <html lang="en">
-<head>
-  <link rel="stylesheet" href="css/style.css">
-</head>
-<style>
-
-    .navTop {
-      position: sticky;
-      top: 0;
-      z-index: 1000;
-      background-color: black;
-      padding: 0.85rem;
-      color: #f8f9fa;
-      display: flex;
-      align-items: center;
-      justify-content: start;
-      padding-left: 3rem;
-    }
-
-    .container {
-      margin: 0;
-      width: 100%;
-      height: auto;
-      padding-left: 0px !important;
-      padding-right: 0px !important;
-      background: url('../images/warehouse.jpg');
-      background-size: cover;
-      display: inline-block;
-      position: relative; 
-      z-index: 1; 
-    }
-
-  .header {
-    text-align: center;
-    background: linear-gradient(to right, #23013a, #380202);
-    padding: 30px;
-    max-width: 100%;
-    color: #ffffff;
-  }
-
-  .container div.formbox {
-    padding: 50px;
-    display:flex;
-    width: 100%;
-    justify-items: center;
-    height: 800px;
-    position: relative;
-  }
-
-  .container div.formform {
-    border-radius: 20px;
-    background-color: #f2f2f2;
-    padding: 20px;
-    width: 30%;
-    font-size: 30px;
-    position: absolute;
-    left:50%;
-    top: 9%;
-  }
-
-  .container ul {
-    list-style-type: disc;
-    color: white;
-  }
-
-  .container li {
-    margin: 50px;
-    text-align: center;
-    display: inline-block;
-  }
-
-  .contantbox {
-    padding: 20px;
-    background: linear-gradient(to right, #23013a, #380202);
-  }
-    .formform input,
-  .formform textarea {
-    width: 100%;
-    padding: 12px 15px;
-    margin-bottom: 20px;
-    border: 1px solid #ccc;
-    border-radius: 8px;
-    font-size: 16px;
-    outline: none;
-    transition: 0.3s;
-  }
-  .formwelcommessagebox {
-    color: white;
-    margin-right: 20px;
-    background-color: rgb(48, 19, 19);
-    border: black 2px solid;
-    padding: 20px;
-    border-radius: 10px;
-    width: 40%;
-    height: 700px;
-    position: absolute;
-    left: 20%;
-  } 
-  .formwelcomemessage {
-    text-align: left;
-    font-size: 20px;
-    margin-bottom: 20px;
-    width:60%;
-    margin-top: 20%;
-  }
-</style>
 
 <head>
+  <title>Contact-Us</title>
+  <link rel="stylesheet" href="../css/style.css">
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <title>WongKokSeng Wholesale</title>
@@ -125,6 +22,7 @@ session_start();
 </head>
 
 
+
 <body>
   <nav class="navTop">
     <a href="index.php">WongKokSeng Wholesale</a>
@@ -136,29 +34,30 @@ session_start();
       <li><a href="adminPanel.php">Admin</a></li>
     </ul>
     <?php if (isset($_SESSION['username'])): ?>
-    <span class="welcome-message">Welcome,
-      <?php echo $_SESSION['username']; ?>!
-    </span>
-    <a class="logout" href="../auth/logout.php">Logout</a>
+      <span class="welcome-message">Welcome,
+        <?php echo $_SESSION['username']; ?>!
+      </span>
+      <a class="logout" href="../auth/logout.php">Logout</a>
     <?php else: ?>
-    <a class="login" href="../auth/login.php">Login / Register</a>
+      <a class="login" href="../auth/login.php">Login / Register</a>
     <?php endif; ?>
   </nav>
 
 
-  <div class="header">
+  <div class="contact-header">
     <h2>Contact Us</h2>
   </div>
-  <div class="container" style="max-width: 100%;">
-    <div class="formbox">
-      <div class="formwelcommessagebox">
-        <div class="formwelcomemessage">
-        <h3>We would love to hear from you!</h3>
-        <p>If you have any questions, suggestions, or feedback, please feel free to reach out to us using the form below.
-          Our team is here to assist you and ensure you have the best experience with WongKokSeng Wholesale.</p>
+  <div class="contact-container" style="max-width: 100%;">
+    <div class="contact-formbox">
+      <div class="contact-form-welcome-message-box">
+        <div class="contact-form-welcome-message">
+          <h3>We would love to hear from you!</h3>
+          <p>If you have any questions, suggestions, or feedback, please feel free to reach out to us using the form
+            below.
+            Our team is here to assist you and ensure you have the best experience with WongKokSeng Wholesale.</p>
         </div>
       </div>
-      <div class="formform">
+      <div class="contact-form">
         <form action="submit_form.php" method="POST">
           <div>
             <label>Name:</label>
@@ -184,7 +83,7 @@ session_start();
         </form>
       </div>
     </div>
-     <div class="contantbox">
+    <div class="contact-info">
       <ul style="text-align: center;">
         <li>
           <p>Email: Wong@gmail.com </p>
--- pages/preorder.php
+++ pages/preorder.php
@@ -1,5 +1,5 @@
 <?php
-include  '../auth/db_connect.php';
+include '../auth/db_connect.php';
 session_start();
 ?>
 
@@ -12,7 +12,7 @@ session_start();
   <title>WongKokSeng Wholesale</title>
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet"
     integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
-<link rel="stylesheet" href="../css/style.css">
+  <link rel="stylesheet" href="../css/style.css">
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js"
     integrity="sha384-FKyoEForCGlyvwx9Hj09JcYn3nv7wiPVlz7YYwJrWVcXK/BmnVDxM+D2scQbITxI"
     crossorigin="anonymous"></script>
@@ -30,7 +30,7 @@ session_start();
       <li><a href="preorder.php" class="active">Preorder</a></li>
       <li><a href="../admin/adminPanel.php">Admin</a></li>
     </ul>
-     <?php if (isset($_SESSION['username'])): ?>
+    <?php if (isset($_SESSION['username'])): ?>
       <span class="welcome-message">Welcome, <?php echo $_SESSION['username']; ?>!</span>
       <a class="logout" href="../auth/logout.php">Logout</a>
     <?php else: ?>
